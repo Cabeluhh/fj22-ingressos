0b52dab03457ea66c84c8c34501542ddc2227299
lab7172 <josehenriquejunior@hotmail.com>
2017-09-01 20:04:27 -0300

    Adicionando Logica da tela de compra de lugares

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,6 +3,8 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -69,4 +71,11 @@ public BigDecimal getPreco() {
 public void setPreco(BigDecimal preco) {
 	this.preco = preco;
 }
+public Map<String, List<Lugar>> getMapaDeLugares(){
+	return sala.getMapaDeLugares();
+}
+public boolean isDisponivel(Lugar lugar){
+	return true;
+}
+
 }
